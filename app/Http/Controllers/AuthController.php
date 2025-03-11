c6fd2c98d570b8af27ddb68bf8d79d370e889bc5
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-03-11 15:01:49 +0000

    add AuthController for user registration

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+}
